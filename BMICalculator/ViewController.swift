5a0af15c9bc26e698f5ce76f8db6bbdb7a2c0a85
Behnam_rakhshani <b.rakhshani77@gmail.com>
2020-11-12 11:06:22 +0330

    Initial Commit

---
+++ BMICalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMICalculator
+//
+//  Created by Behnam on 11/12/20.
+//  Copyright © 2020 BehnamR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
